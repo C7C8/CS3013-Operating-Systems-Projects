f50133d8bdefc8ab4483f200b776fff0627089bd
Sourec <crmyers@wpi.edu>
2017-01-23 19:29:51 -0500

    Implement linked list for commands, fix all command execution
    Commands are now stored in a linked list instead of an array. This
    sacrifices a little bit of efficiency (command execution now runs
    in O(n) time instead of O(1), but it is ultimately safer and much
    easier to work with.

--- project1/v1/mc1.c
+++ project1/v1/mc1.c
@@ -6,33 +6,41 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
-	char** cmd;
-} scmd;
+typedef struct linkedCmd linkedCmd;
+struct linkedCmd {
+	char* args[10];
+	linkedCmd* next;
+};
 
-
-void execCMD(scmd cmd);
+void execCmd(linkedCmd* cmd);
+void addCmd(linkedCmd* root, linkedCmd* newCmd);
+void freeCmdList(linkedCmd* root);
+linkedCmd* getCmd(linkedCmd* root, int pos);
 
 int main(){
-	//Would a char*** work? Absolutely, but this is way more fun!
-	scmd cmdlist[10] = {	{{ "whoami", NULL}},
-				{{ "last", NULL}},
-				{{ "ls", "--color=tty", NULL}},
-				{{ NULL }},
-				{{ NULL }},
-				{{ NULL }}, //ugh, at least it works
-				{{ NULL }},
-				{{ NULL }},
-				{{ NULL }},
-				{{ NULL }}};
+	linkedCmd* cmdList = (linkedCmd*)malloc(sizeof(linkedCmd));
+	addCmd(cmdList, (linkedCmd*)malloc(sizeof(linkedCmd)));
+	addCmd(cmdList, (linkedCmd*)malloc(sizeof(linkedCmd)));
+
+	//Have to manually assign these values using strcpy. There might be a better way, but
+	//right now I'm feeling too lazy to do anything about it.
+	getCmd(cmdList, 0)->args[0] = strdup("whoami");
+	getCmd(cmdList, 1)->args[0] = strdup("last");
+	getCmd(cmdList, 2)->args[0] = strdup("ls"); getCmd(cmdList, 2)->args[1] = strdup("--color=tty");
+	
 	int cmdcount = 3;
 
 	while (1) {
 		printf("\n===== Mid-Day Commander, v0 =====\n"); //otherwise known as fake-bash...
 		printf("G'day, Commander! What command would you like to run?\n");
 		
-		for (int i = 0; i < cmdcount; i++)
-			printf("\t%d. %s\n", i, cmdlist[i]);
+		for (int i = 0; i < cmdcount; i++){
+			char** args = getCmd(cmdList, i)->args;
+			printf("\t%d. ", i);
+			for (int j = 0; args[j] != NULL; j++)
+				printf("%s ", args[j]);
+			printf("\n");
+		}
 
 		printf("\ta. add command : Adds a new commmand to the menu.\n");
 		printf("\tc. change directory : Changes process working directory\n");
@@ -41,12 +49,12 @@ int main(){
 		printf("Option?: ");
 		char option = getchar(); //TODO: Change input method to allow for better number input
 		getchar(); //Skip the newline. This is hacky, but it works. Just don't do CTRL-D.
+		printf("\n");
 
 		//Who needs atoi anyways? Check if input is a number, verify that it's a valid command
 		//if it is, and then execute said command.
 		if (option >= 48 && option <= 57 && option - 48 < cmdcount){
-			printf("Executing command #%d!\n", option - 48);
-			execCMD(cmdlist[option - 48]);
+			execCmd(getCmd(cmdList, option - 48));
 			continue;
 		}
 
@@ -56,15 +64,17 @@ int main(){
 				continue;
 			case 'c' :
 				printf("What directory would you like to change to, Commander?: ");
-				long size = 1024;
+				unsigned long size = 1024;
 				char* newWD = (char*)malloc(size);
 				getline(&newWD, &size, stdin);
 				newWD[strlen(newWD)-1] = '\0'; //chop off the newline
-				printf("Changing directory to \"%s\"...\n", newWD);
 				chdir((const char*)newWD);
 				free(newWD);
 				continue;
 			case 'e' :
+				printf("Goodbye, Commander...\n");
+				printf("--crmyers 2017\n");
+				freeCmdList(cmdList);
 				exit(0);
 			case 'p' : { //Braces needed because we're declaring a new variable
 				char* cwd = getcwd(NULL, 0);
@@ -79,17 +89,43 @@ int main(){
 	return 0;
 }
 
-void execCMD(scmd cmd){
+linkedCmd* getCmd(linkedCmd* root, int pos){
+	//Get a CMD in an arbitrary position in the list. Useful because
+	//we can't recurse very easily in the main menu.
+	if (root == NULL)
+		return NULL; 
 
-	//printf("Trying to execute command as follows:\n");
-	//for (int i = 0; args[i] != NULL; i++)
-	//	printf("%s\n", args[i]);
+	if (pos == 0)
+		return root;
+	return getCmd(root->next, pos - 1);
+}
+
+void addCmd(linkedCmd* root, linkedCmd* newCmd){
+	//Add a cmd to the end of the list. There is no deleting of cmds;
+	//why would you WANT that?
+	if (root->next == NULL){
+		newCmd->next = NULL;
+		root->next = newCmd;
+		return;
+	}
+	addCmd(root->next, newCmd);
+}
+
+void freeCmdList(linkedCmd* root){
+	//Recursively free the cmdList
+	if (root == NULL)
+		return;
+	freeCmdList(root->next);
+	free(root);
+}
+
+void execCmd(linkedCmd* cmd){
 	struct timeval ctime;
 	gettimeofday(&ctime, NULL);
 
 	int pid = fork();
 	if (pid == 0){
-		execvp(cmd.cmd[0], cmd.cmd);
+		execvp(cmd->args[0], (char** const)cmd->args);
 	}
 	else {
 		struct rusage usage;
@@ -98,7 +134,7 @@ void execCMD(scmd cmd){
 		struct timeval ntime;
 		gettimeofday(&ntime, NULL);
 		float timeDelta = (float)(ntime.tv_usec - ctime.tv_usec) / 1000.0;
-		printf("Elapsed time: %f ms\n", timeDelta);
+		printf("\nElapsed time: %f ms\n", timeDelta);
 		printf("\nPage faults: %d\n", (int)usage.ru_majflt);
 		printf("Page faults (reclaimed): %d\n\n", (int)usage.ru_minflt);
 	}
